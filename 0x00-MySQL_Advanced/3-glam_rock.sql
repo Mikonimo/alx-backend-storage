6f3e95ed8d409d4fa7fcd7d54aaa2262563e947d
mikonimo <mikonani413@gmail.com>
2025-02-03 12:29:02 +0300

    refactor SQL script to simplify lifespan calculation for Glam rock bands

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,19 +1,10 @@
 -- Lists all bands with Glam rock as their main style, ranked by their longevity
 -- Requirements:
 -- Import this table dump: metal_bands.sql.zip
--- Column names must be: band_name and lifespan (in years until 2022)
+-- Column names must be: band_name and lifespan (in years until 2022 - please use 
+-- 2022 instead of YEAR(CURDATE()))
 -- You should use attributes formed and split for computing the lifespan
 -- The script can be executed on any database
-
-SELECT
-    band_name,
-    IFNULL(
-            CASE
-                WHEN split IS NULL THEN split - formed
-                ELSE 2022 - formed
-            END,
-            0
-    ) as lifespan
-FROM metal_bands
-WHERE FIND_IN_SET('Glam rock', style)
-ORDER BY lifespan DESC;
+SELECT band_name,
+        COALESCE(split, 2022) - formed as lifespan FROM  metal_bands
+        WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
