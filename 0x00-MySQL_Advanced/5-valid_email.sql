1aa7fec621dabfcd6005a37b6837f0e7df6bf880
mikonimo <mikonani413@gmail.com>
2025-02-03 12:42:27 +0300

    add delimiter statement for reset_valid_email trigger in valid_email.sql

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,5 +1,6 @@
 -- Creates a trigger that resets the attribute valid_email only when the
 -- email has been changed
+DELIMITER $$ ;
 CREATE TRIGGER reset_valid_email
 BEFORE UPDATE ON users
 FOR EACH ROW
@@ -8,3 +9,4 @@ BEGIN
     SET NEW.valid_email = 0;
   END IF;
 END;
+DELIMITER
