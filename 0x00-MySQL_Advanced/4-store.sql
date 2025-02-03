86e6c22b113f7319144c898220ac86e60227ec40
mikonimo <mikonani413@gmail.com>
2025-02-03 12:23:15 +0300

    fix trigger to correctly decrease item quantity after a new order is placed

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,15 +1,9 @@
 -- Creates a trigger that decreases the quantity of an item after
 -- adding a new order.
 -- Quantity in the table items can be negative.
-DELIMITER $$
-
-CREATE TRIGGER decrease_quantiy_after_order
+CREATE TRIGGER decrease_quantity
 AFTER INSERT ON orders
-FOR EACH ROW
-BEGIN
-    UPDATE items
-    SET quantity = quantity = NEW.number
-    WHERE name = NEW.item_name;
-END$$
-
-DELIMITER ;
+FORE EACH ROW
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
