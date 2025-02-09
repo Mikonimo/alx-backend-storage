ec0970bf99756bc5ed839a9f3f14f008d8866118
mikonimo <mikonani413@gmail.com>
2025-02-09 12:22:17 +0300

    add function SafeDiv to safely divide two integers, returning 0 if the divisor is zero

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,20 @@
+-- Create a function SfeDiv that divides (and returns) the first by the second number
+-- or returns 0 if the second number is equal to 0.
+-- Requirements"
+-- - You must create a function
+-- - The function SafeDiv takes 2 arguments: a and b (both integers)
+-- - And returns a / b or 0 if b == 0
+
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS INT
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    ELSE
+        RETURN a / b;
+    END IF;
+END$$
+
+DELIMITER ;
