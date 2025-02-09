6ce72602ebfbb30c88d1d9129e150a34b6f732c3
mikonimo <mikonani413@gmail.com>
2025-02-09 12:30:28 +0300

    refactor need_meeting view to select from students table and simplify conditions

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -5,9 +5,7 @@
 -- - Their score are under (strict) to 80
 -- - AND no last_meeting date OR more that 1 month
 
+DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS
-SELECT users.name
-FROM users
-JOIN corrections ON users.id = corrections.user_id
-WHERE corrections.score < 80
-AND (users.last_meeting IS NULL OR users.last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH));
+SELECT name FROM students WHERE score < 80
+AND (students.last_meeting IS NULL OR students.last_meeting < DATE_ADD(NOW(), INTERVAL -1 MONTH));
