e2cc654b336fca602cdcf1a875bd64ede66f3f79
mikonimo <mikonani413@gmail.com>
2025-02-09 11:43:56 +0300

    refactor ComputeAverageScoreForUser procedure to use proper syntax and improve readability

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -5,12 +5,15 @@
 -- user_id, a users.id value (you can assume user_id is linked to a an existing users)
 
 DELIMITER $$
+
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
-CREATE PROCEDURE ComputeAverageScoreForUser(user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-    UPDATE users
-    SELECT
-    average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
-    WHERE id = user_id;
-END;$$
+	UPDATE users
+	SET
+	average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+	WHERE id = user_id;
+
+END $$
+
 DELIMITER ;
